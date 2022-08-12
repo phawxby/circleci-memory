bc0f41283ed5b369b4e757475f9ce5f4556a9522
Paul Hawxby <phawxby@gmail.com>
2022-08-12 10:16:11 +0100

    test: add workflow for circleci and github

---
+++ info.js
@@ -0,0 +1,44 @@
+const os = require('os');
+
+/**
+ * Format bytes as human-readable text.
+ * 
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use 
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ * 
+ * @remarks
+ * https://stackoverflow.com/a/14919494/468214
+ * 
+ * @return Formatted string.
+ */
+ function humanFileSize(bytes, si=false, dp=1) {
+    const thresh = si ? 1000 : 1024;
+  
+    if (Math.abs(bytes) < thresh) {
+      return bytes + ' B';
+    }
+  
+    const units = si 
+      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10**dp;
+  
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+  
+  
+    return bytes.toFixed(dp) + ' ' + units[u];
+  }
+
+console.log({
+    totalMem: os.totalmem(),
+    totalMemFormatted: humanFileSize(os.totalmem()),
+    freeMem: os.freemem(),
+    freeMemFormatted: humanFileSize(os.freemem()),
+    cpus: os.cpus().length
+})
